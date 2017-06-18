9234fa07a6265d8217c6f261245fb828db4e9910
Katelin Zichittella <katelinz2@frontier.com>
2017-06-18 17:30:51 -0400

    fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousHeaderNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousHeaderNew.java
@@ -20,52 +20,155 @@ public abstract class AutonomousHeaderNew extends LinearOpMode {
         motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         motorRight.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Initialized", true);
+        telemetry.update();
     }
 
-    public void cosineEncodersForward(int distance, double power) {
+    // Lower speeds need higher threshold values, but are best
 
-        int DIAMETER = 4;
-        int GEAR_RATIO = 1;
-        int PULSES = 1120;
-        double CIRCUMFERENCE = Math.PI * DIAMETER;
-        double ROTATIONS = (distance / CIRCUMFERENCE) * GEAR_RATIO;
-        double COUNTS = PULSES * ROTATIONS;
+    public void cosineEncodersForward(int distance, double power, double threshold) {
 
-        motorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        if (opModeIsActive()) {
 
-        setMotorPower(power, power);
+            int DIAMETER = 4;
+            int GEAR_RATIO = 1;
+            int PULSES = 1120;
+            double CIRCUMFERENCE = Math.PI * DIAMETER;
+            double ROTATIONS = (distance / CIRCUMFERENCE) * GEAR_RATIO;
+            double COUNTS = PULSES * ROTATIONS;
 
-        while (motorLeft.getCurrentPosition() < COUNTS) {
+            motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            int i = motorLeft.getCurrentPosition();
-            setMotorPower(((-Math.cos(i * ((2 * Math.PI) / COUNTS)) * power)) + power,
-                    ((-Math.cos(i * ((2 * Math.PI) / COUNTS))) * power) + power);
+            setMotorPower(power, power);
+
+            while (motorRight.getCurrentPosition() < COUNTS && opModeIsActive()) {
+
+                int i = motorRight.getCurrentPosition();
+
+                setMotorPower((-(power / 2) * (Math.cos((i + threshold) * ((2 * Math.PI) / (COUNTS + (threshold * 2)))))) + (power / 2),
+                        (-(power / 2) * (Math.cos((i + threshold) * ((2 * Math.PI) / (COUNTS + (threshold * 2)))))) + (power / 2));
+
+                telemetry.addData("In While Loop", true);
+                telemetry.addData("COUNTS", COUNTS);
+                telemetry.addData("Current Position", motorRight.getCurrentPosition());
+                telemetry.addData("i", i);
+                telemetry.addData("Left Speed", motorLeft.getPower());
+                telemetry.addData("Right Speed", motorRight.getPower());
+                telemetry.update();
+            }
+
+            setMotorPower(0, 0);
         }
-
-        setMotorPower(0, 0);
     }
 
-    public void cosineEncodersBackward(int distance, double power) {
+    public void halfCosineEncodersForward(int distance, double power, double threshold) { // Needs a slightly higher threshold, but is best
 
-        int DIAMETER = 4;
-        int GEAR_RATIO = 1;
-        int PULSES = 1120;
-        double CIRCUMFERENCE = Math.PI * DIAMETER;
-        double ROTATIONS = (distance / CIRCUMFERENCE) * GEAR_RATIO;
-        double COUNTS = PULSES * ROTATIONS;
+        if (opModeIsActive()) {
 
-        motorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            int DIAMETER = 4;
+            int GEAR_RATIO = 1;
+            int PULSES = 1120;
+            double CIRCUMFERENCE = Math.PI * DIAMETER;
+            double ROTATIONS = (distance / CIRCUMFERENCE) * GEAR_RATIO;
+            double COUNTS = PULSES * ROTATIONS;
 
-        setMotorPower(-power, -power);
+            motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        while (motorLeft.getCurrentPosition() > COUNTS) {
+            setMotorPower(power, power);
 
-            int i = motorLeft.getCurrentPosition();
-            setMotorPower(-((-Math.cos(-i * ((2 * Math.PI) / COUNTS)) * power) + power),
-                    -((-Math.cos(-i * ((2 * Math.PI) / COUNTS)) * power) + power));
+            while (motorRight.getCurrentPosition() < COUNTS && opModeIsActive()) {
+
+                int i = motorRight.getCurrentPosition();
+
+                setMotorPower((-(power / 2) * (Math.cos((i + (((COUNTS * 2) + threshold) / 2)) * ((2 * Math.PI) / ((COUNTS * 2) + (threshold * 2)))))) + (power / 2),
+                        (-(power / 2) * (Math.cos((i + (((COUNTS * 2) + threshold) / 2)) * ((2 * Math.PI) / ((COUNTS * 2) + (threshold * 2)))))) + (power / 2));
+
+                telemetry.addData("In While Loop", true);
+                telemetry.addData("COUNTS", COUNTS);
+                telemetry.addData("Current Position", motorRight.getCurrentPosition());
+                telemetry.addData("i", i);
+                telemetry.addData("Left Speed", motorLeft.getPower());
+                telemetry.addData("Right Speed", motorRight.getPower());
+                telemetry.update();
+            }
+
+            setMotorPower(0, 0);
         }
+    }
 
-        setMotorPower(0, 0);
+    public void cosineEncodersBackward(int distance, double power, double threshold) {
+
+        if (opModeIsActive()) {
+
+            int DIAMETER = 4;
+            int GEAR_RATIO = 1;
+            int PULSES = 1120;
+            double CIRCUMFERENCE = Math.PI * DIAMETER;
+            double ROTATIONS = (distance / CIRCUMFERENCE) * GEAR_RATIO;
+            double COUNTS = PULSES * ROTATIONS;
+
+            motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            setMotorPower(-power, -power);
+
+            while (-motorRight.getCurrentPosition() < COUNTS && opModeIsActive()) {
+
+                int i = -motorRight.getCurrentPosition();
+
+                setMotorPower(-((-(power / 2) * (Math.cos((i + threshold) * ((2 * Math.PI) / (COUNTS + (threshold * 2)))))) + (power / 2)),
+                        -((-(power / 2) * (Math.cos((i + threshold) * ((2 * Math.PI) / (COUNTS + (threshold * 2)))))) + (power / 2)));
+
+                telemetry.addData("In While Loop", true);
+                telemetry.addData("COUNTS", COUNTS);
+                telemetry.addData("Current Position", -motorRight.getCurrentPosition());
+                telemetry.addData("i", i);
+                telemetry.addData("Left Speed", motorLeft.getPower());
+                telemetry.addData("Right Speed", motorRight.getPower());
+                telemetry.update();
+            }
+
+            setMotorPower(0, 0);
+        }
+    }
+
+    public void halfCosineEncodersBackward(int distance, double power, double threshold) { // Needs a slightly higher threshold, but is best
+
+        if (opModeIsActive()) {
+
+            int DIAMETER = 4;
+            int GEAR_RATIO = 1;
+            int PULSES = 1120;
+            double CIRCUMFERENCE = Math.PI * DIAMETER;
+            double ROTATIONS = (distance / CIRCUMFERENCE) * GEAR_RATIO;
+            double COUNTS = PULSES * ROTATIONS;
+
+            motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            setMotorPower(-power, -power);
+
+            while (-motorRight.getCurrentPosition() < COUNTS && opModeIsActive()) {
+
+                int i = -motorRight.getCurrentPosition();
+
+                setMotorPower(-((-(power / 2) * (Math.cos((i + (((COUNTS * 2) + threshold) / 2)) * ((2 * Math.PI) / ((COUNTS * 2) + (threshold * 2)))))) + (power / 2)),
+                        -((-(power / 2) * (Math.cos((i + (((COUNTS * 2) + threshold) / 2)) * ((2 * Math.PI) / ((COUNTS * 2) + (threshold * 2)))))) + (power / 2)));
+
+                telemetry.addData("In While Loop", true);
+                telemetry.addData("COUNTS", COUNTS);
+                telemetry.addData("Current Position", -motorRight.getCurrentPosition());
+                telemetry.addData("i", i);
+                telemetry.addData("Left Speed", motorLeft.getPower());
+                telemetry.addData("Right Speed", motorRight.getPower());
+                telemetry.update();
+            }
+
+            setMotorPower(0, 0);
+        }
     }
 
     public void setMotorPower(double left, double right) {
@@ -74,3 +177,22 @@ public abstract class AutonomousHeaderNew extends LinearOpMode {
         motorRight.setPower(right);
     }
 }
+/*for (int i = 500; i < COUNTS; i++) {
+
+                motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+                while (motorRight.getCurrentPosition() < 1) {
+
+                    setMotorPower((-0.5 * (Math.cos(i * ((2 * Math.PI) / COUNTS) * power))) + (power / 2),
+                            (-0.5 * (Math.cos(i * ((2 * Math.PI) / COUNTS) * power))) + (power / 2));
+
+                    telemetry.addData("In Loop", true);
+                    telemetry.addData("COUNTS", COUNTS);
+                    telemetry.addData("Current Position", motorRight.getCurrentPosition());
+                    telemetry.addData("i", i);
+                    telemetry.addData("Left Speed", motorLeft.getPower());
+                    telemetry.addData("Right Speed", motorRight.getPower());
+                    telemetry.update();
+                }
+            }*/
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousNew.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
  */
 
 @Autonomous
-@Disabled
+//@Disabled
 
 public class AutonomousNew extends AutonomousHeaderNew {
 
@@ -19,16 +19,52 @@ public class AutonomousNew extends AutonomousHeaderNew {
 
         waitForStart();
 
-        cosineEncodersForward(40, 1.0);
+        cosineEncodersForward(40, 1.0, 250);
         Thread.sleep(1000);
 
-        cosineEncodersBackward(40, 1.0);
+        halfCosineEncodersForward(40, 1.0, 300);
         Thread.sleep(1000);
 
-        /*cosineEncodersForward(40, 0.5);
+        cosineEncodersForward(40, 0.5, 450);
         Thread.sleep(1000);
 
-        cosineEncodersBackward(40, 0.5);
-        Thread.sleep(1000);*/
+        halfCosineEncodersForward(40, 0.5, 600);
+        Thread.sleep(1000);
+
+        cosineEncodersForward(6, 1.0, 45);
+        Thread.sleep(1000);
+
+        halfCosineEncodersForward(6, 1.0, 50);
+        Thread.sleep(1000);
+
+        cosineEncodersForward(6, 0.5, 65);
+        Thread.sleep(1000);
+
+        halfCosineEncodersForward(6, 0.5, 70);
+        Thread.sleep(1000);
+
+        cosineEncodersBackward(40, 1.0, 250);
+        Thread.sleep(1000);
+
+        halfCosineEncodersBackward(40, 1.0, 300);
+        Thread.sleep(1000);
+
+        cosineEncodersBackward(40, 0.5, 450);
+        Thread.sleep(1000);
+
+        halfCosineEncodersBackward(40, 0.5, 600);
+        Thread.sleep(1000);
+
+        cosineEncodersBackward(6, 1.0, 45);
+        Thread.sleep(1000);
+
+        halfCosineEncodersBackward(6, 1.0, 50);
+        Thread.sleep(1000);
+
+        cosineEncodersBackward(6, 0.5, 65);
+        Thread.sleep(1000);
+
+        halfCosineEncodersBackward(6, 0.5, 70);
+        Thread.sleep(1000);
     }
 }
