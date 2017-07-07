3ccc3a1ab9ddd287f9bb568f69369bf793ad190f
Katelin Zichittella <katelinz2@frontier.com>
2017-07-07 14:09:49 -0400

    fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousHeaderNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousHeaderNew.java
@@ -81,10 +81,16 @@ public abstract class AutonomousHeaderNew extends LinearOpMode {
 
                 int currentEncoderCount = motorRight.getCurrentPosition();
 
-                setMotorPower((-((maxPower - 0.1) / 2) * (Math.cos((currentEncoderCount + COUNTS) *
-                                ((2 * Math.PI) / (COUNTS * 2))))) + ((maxPower + 0.1) / 2),
-                        (-((maxPower - 0.1) / 2) * (Math.cos((currentEncoderCount + COUNTS) *
-                                ((2 * Math.PI) / (COUNTS * 2))))) + ((maxPower + 0.1) / 2));
+                setMotorPower((-((maxPower - 0.1) / 2) * (Math.cos((currentEncoderCount + COUNTS) * ((2 * Math.PI) / (COUNTS * 2))))) + ((maxPower + 0.1) / 2),
+                        (-((maxPower - 0.1) / 2) * (Math.cos((currentEncoderCount + COUNTS) * ((2 * Math.PI) / (COUNTS * 2))))) + ((maxPower + 0.1) / 2));
+
+                telemetry.addData("In While Loop", true);
+                telemetry.addData("COUNTS", COUNTS);
+                telemetry.addData("Current Position", motorRight.getCurrentPosition());
+                telemetry.addData("currentEncoderCount", currentEncoderCount);
+                telemetry.addData("Left Speed", motorLeft.getPower());
+                telemetry.addData("Right Speed", motorRight.getPower());
+                telemetry.update();
             }
 
             setMotorPower(0, 0);
