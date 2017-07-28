55f0c89b26fcbf7f383b8cb7e5f17b34ba7cdfe1
Katelin Zichittella <katelinz2@frontier.com>
2017-07-28 17:55:47 -0400

    fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousHeaderPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousHeaderPractice.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDevice;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
 /**
@@ -24,7 +26,13 @@ public abstract class AutonomousHeaderPractice extends LinearOpMode {
 
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
+
         sensorColor = hardwareMap.i2cDevice.get("sensorColor");
+        sensorColorReader = new I2cDeviceSynchImpl(sensorColor, I2cAddr.create8bit(0x42), false);
+        // I2c address of this color sensor
+        sensorColorReader.write8(3, 0); // LED on (off would be 1)
+        sensorColorReader.engage();
+
         sensorODS = hardwareMap.opticalDistanceSensor.get("sensorODS");
 
         motorRight.setDirection(DcMotor.Direction.REVERSE);
@@ -33,17 +41,43 @@ public abstract class AutonomousHeaderPractice extends LinearOpMode {
         telemetry.update();
     }
 
-    public void shortDis (int distance) {
+    public void stopAtDistance(double distance, double power) { // Distance is 0 for farthest away
+                                                                // and 1 for closest
+        if (opModeIsActive()) {
 
-        motorLeft.setPower(1.0);
-        motorRight.setPower(1.0);
+            motorLeft.setPower(power);
+            motorRight.setPower(power);
 
-        while (sensorODS.getLightDetected() < 100) {
+            while (sensorODS.getLightDetected() < distance && opModeIsActive()) {
 
+                telemetry.addData("Normal", sensorODS.getLightDetected());
+                telemetry.update();
+            }
 
+            motorLeft.setPower(0.0);
+            motorRight.setPower(0.0);
         }
+    }
 
-        motorLeft.setPower(0.0);
-        motorRight.setPower(0.0);
+    public void moveForwardWhenRed(double power) {
+
+        while (opModeIsActive()) {
+
+            sensorColorCache = sensorColorReader.read(0x04, 1); // Color Number
+
+            telemetry.addData("Color Number", sensorColorCache[0] & 0xFF);
+            telemetry.update();
+
+            if ((sensorColorCache[0] & 0xFF) > 10 && (sensorColorCache[0] & 0xFF) < 12) { // Sees Red
+
+                motorLeft.setPower(power);
+                motorRight.setPower(power);
+            }
+            else {
+
+                motorLeft.setPower(0.0);
+                motorRight.setPower(0.0);
+            }
+        }
     }
 }
