64fb231bbf2f08249fe06791caeb4ab374295482
Alex <albobby@hvc.rr.com>
2017-10-08 15:08:05 -0400

    moved content from JewelAutonomous (nick's program) to RoboRaiderAuto so now the former extends the latter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelAutonomous.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.roboraiders.Robot.RoboRaiderAuto;
 import com.roboraiders.Robot.Robot;
 
 
@@ -10,7 +11,10 @@ import com.roboraiders.Robot.Robot;
  * Created by (mostly) Nick Urbin and (a little bit) Kevin McCrudden on 10/1/17.
  */
 
-  class JewelAutonomous extends LinearOpMode {
+  class JewelAutonomous extends RoboRaiderAuto {
+
+    int red = 1;
+    int blue = 2;
 
     public Robot robot = new Robot();
 
@@ -25,56 +29,9 @@ import com.roboraiders.Robot.Robot;
         robot.colorSensor.red();
         robot.colorSensor.blue();
 
-        telemetry.addData("Red", robot.colorSensor.red());
-        telemetry.addData("Blue", robot.colorSensor.blue());
-        telemetry.update();
-// does the robot need to move forward at all? or no? discuss with program team. this programs assumes no.
-        robot.servoJewel.setPosition(0.5);// lower arm with color sensor
+        SelectJewel(robot, red);
 
-        //assuming color sensor is mounted facing right
 
-        //assuming red alliance
-
-        // if (allianceColorRed == true){
-        if (robot.colorSensor.red() > 675 &&  robot.colorSensor.red() <= 775) { // ball on the right is red
-            //using motor power until we get math figured out for encoders
-
-            robot.setDriveMotorPower(-1,1,1,-1); //strafe left
-            Thread.sleep (500);
-            robot.setDriveMotorPower(0,0,0,0);
-            Thread.sleep (500);
-            robot.setDriveMotorPower(1, -1, -1, 1); //strafe right to original position
-            Thread.sleep (500);
-            }
-        else { //if the ball on the right is blue
-            robot.setDriveMotorPower(1, -1, -1, 1); //strafe right
-            Thread.sleep (500);
-            robot.setDriveMotorPower(0,0,0,0);
-            Thread.sleep (500);
-            robot.setDriveMotorPower(-1,1,1,-1); //strafe left to original position
-            Thread.sleep (500);
-
-        }
-        //}
-
-        //assuming blue alliance
-
-        //if {allianceColorRed == false){               // therefore blue alliance
-        if (robot.colorSensor.blue() <= 675  && robot.colorSensor.blue() >= 575) {      // ball on the right is blue
-            robot.setDriveMotorPower(-1, 1, 1, -1); //strafe left
-            Thread.sleep(500);
-            robot.setDriveMotorPower(0, 0, 0, 0);
-            Thread.sleep(500);
-            robot.setDriveMotorPower(1, -1, -1, 1); //strafe right to original position
-            Thread.sleep(500);
-            }
-        else {
-            robot.setDriveMotorPower(1, -1, -1, 1); //strafe right
-            Thread.sleep (500);
-            robot.setDriveMotorPower(0,0,0,0);
-            Thread.sleep (500);
-            robot.setDriveMotorPower(-1,1,1,-1); //strafe left to original position
-            Thread.sleep (500);
         }
 
 
@@ -82,4 +39,4 @@ import com.roboraiders.Robot.Robot;
 
         //}
     }
-}
+
